54d721b786fc9951385d01cc9c1bdacb1c702658
adunukotaashok-2007 <adunukotaashok@gmail.com>
2026-08-26 19:17:12 +0530

    Initialize Phaser game with scenes and config

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import Phaser from "phaser";
+
+import BootScene from "./scenes/BootScene.js";
+import MenuScene from "./scenes/MenuScene.js";
+import LevelSelectScene from "./scenes/LevelSelectScene.js";
+import GameScene from "./scenes/GameScene.js";
+
+const config = {
+  type: Phaser.AUTO,
+
+  width: 960,
+  height: 540,
+
+  parent: "game",
+
+  backgroundColor: "#151515",
+
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 1000
+      },
+      debug: false
+    }
+  },
+
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+
+  scene: [
+    BootScene,
+    MenuScene,
+    LevelSelectScene,
+    GameScene
+  ]
+};
+
+new Phaser.Game(config);
