6e213e880ac6d4283ba2a1418b532d91951a4b9e
adunukotaashok-2007 <adunukotaashok@gmail.com>
2026-08-26 19:17:53 +0530

    Implement BootScene with texture creation

---
+++ src/scenes/BootScene.js
@@ -0,0 +1,59 @@
+import Phaser from "phaser";
+
+export default class BootScene extends Phaser.Scene {
+  constructor() {
+    super("BootScene");
+  }
+
+  create() {
+    this.createTextures();
+
+    this.scene.start("MenuScene");
+  }
+
+  createTextures() {
+    // Player
+    const playerGraphics = this.make.graphics({
+      x: 0,
+      y: 0,
+      add: false
+    });
+
+    playerGraphics.fillStyle(0x4ade80);
+    playerGraphics.fillRect(0, 0, 32, 42);
+    playerGraphics.generateTexture("player", 32, 42);
+    playerGraphics.destroy();
+
+    // Platform
+    const platformGraphics = this.make.graphics({
+      x: 0,
+      y: 0,
+      add: false
+    });
+
+    platformGraphics.fillStyle(0x555555);
+    platformGraphics.fillRect(0, 0, 80, 20);
+    platformGraphics.generateTexture("platform", 80, 20);
+    platformGraphics.destroy();
+
+    // Spike
+    const spikeGraphics = this.make.graphics({
+      x: 0,
+      y: 0,
+      add: false
+    });
+
+    spikeGraphics.fillStyle(0xff3333);
+
+    spikeGraphics.beginPath();
+    spikeGraphics.moveTo(0, 30);
+    spikeGraphics.lineTo(15, 0);
+    spikeGraphics.lineTo(30, 30);
+    spikeGraphics.closePath();
+
+    spikeGraphics.fillPath();
+
+    spikeGraphics.generateTexture("spike", 30, 30);
+    spikeGraphics.destroy();
+  }
+}
