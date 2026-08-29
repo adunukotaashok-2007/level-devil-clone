3c8f9ae49e561b4ddf0f902bcc125f67006968d2
adunukotaashok-2007 <adunukotaashok@gmail.com>
2026-08-29 08:07:29 +0530

    Refactor texture generation for player, platform, and spike

--- src/scenes/bootscene.js
+++ src/scenes/bootscene.js
@@ -12,48 +12,82 @@ export default class BootScene extends Phaser.Scene {
   }
 
   createTextures() {
-    // Player
+
+    // =========================
+    // PLAYER
+    // =========================
+
     const playerGraphics = this.make.graphics({
       x: 0,
       y: 0,
       add: false
     });
 
+    playerGraphics.clear();
     playerGraphics.fillStyle(0x4ade80);
     playerGraphics.fillRect(0, 0, 32, 42);
-    playerGraphics.generateTexture("player", 32, 42);
+
+    playerGraphics.generateTexture(
+      "player",
+      32,
+      42
+    );
+
     playerGraphics.destroy();
 
-    // Platform
+
+    // =========================
+    // PLATFORM
+    // =========================
+
     const platformGraphics = this.make.graphics({
       x: 0,
       y: 0,
       add: false
     });
 
+    platformGraphics.clear();
     platformGraphics.fillStyle(0x555555);
     platformGraphics.fillRect(0, 0, 80, 20);
-    platformGraphics.generateTexture("platform", 80, 20);
+
+    platformGraphics.generateTexture(
+      "platform",
+      80,
+      20
+    );
+
     platformGraphics.destroy();
 
-    // Spike
+
+    // =========================
+    // SPIKE
+    // =========================
+
     const spikeGraphics = this.make.graphics({
       x: 0,
       y: 0,
       add: false
     });
 
+    spikeGraphics.clear();
     spikeGraphics.fillStyle(0xff3333);
 
     spikeGraphics.beginPath();
+
     spikeGraphics.moveTo(0, 30);
     spikeGraphics.lineTo(15, 0);
     spikeGraphics.lineTo(30, 30);
+
     spikeGraphics.closePath();
 
     spikeGraphics.fillPath();
 
-    spikeGraphics.generateTexture("spike", 30, 30);
+    spikeGraphics.generateTexture(
+      "spike",
+      30,
+      30
+    );
+
     spikeGraphics.destroy();
   }
 }
